218286b394ffbdc8a989a46917cd2a55fa1fb42f
Gabrielenke <Gabrielenkee@icloud.com>
2023-07-31 13:42:21 -0300

    add a vercel.json

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -4,13 +4,13 @@ import { astronaut, RectangleSkills } from '../images/skillsImg/export';
 const Skills = () => {
   return (
     <section className=" flex min-h-screen pb-32   text-dark dark:text-light">
-      <div className="flex flex-col md:ml-32 md:mt-32 md:w-[40vw] md:gap-5">
+      <div className="flex flex-col md:ml-32 md:mt-32 md:w-[40vw]  md:gap-5">
         <h3 className=" font-RobotoSlab text-9xl font-black  md:text-[220px]">
           Skills.
         </h3>
         <p></p>
         {/* <img className=" mt-16 w-36 font-bold" src={tagWeb} alt="" /> */}
-        <section className="mx-auto mt-10 flex max-w-[80vw] flex-wrap items-center justify-around gap-10 md:mx-0 md:mt-0 md:w-auto md:flex-col md:items-start md:justify-start  md:gap-28">
+        <section className="mx-auto mt-10 flex max-w-[80vw] flex-wrap items-center justify-around gap-10 md:mx-0 md:mt-0 md:w-auto  md:items-start md:justify-start  md:gap-28">
           {skillsImages.map((skill) => {
             return (
               <div className="flex flex-col items-center justify-center gap-10 md:flex-row md:items-start md:justify-start md:gap-4 ">
